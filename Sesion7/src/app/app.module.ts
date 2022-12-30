8fa6524cc91d2b0c72f9a080deac438fc85b24e5
mersaucedo <mersaucedosantos@gmail.com>
2022-12-29 19:29:41 -0500

    navbar

--- Sesion7/src/app/app.module.ts
+++ Sesion7/src/app/app.module.ts
@@ -3,14 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import {NavbarModule } from './navbar/navbar.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
